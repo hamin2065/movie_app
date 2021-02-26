d2bd40bcc46eae7fbb7bcbfe34ddb208fd67539c
구하민 <hamin2065@gmail.com>
2021-02-26 18:27:09 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component{
-  state = {
-    isLoading: true,
-    movies: []
-  };
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: 
-          { movies }
-      }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-    // 첫번째는 state에서 온 movies이고 두 번째는 axios에서 온 movies이다.같은 movies이기 때문에 줄여서 { movies }라고 써준다.
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount(){
-    this.getMovies(); 
-  }
-
-  render(){
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="loader">
-        {isLoading ? (
-          <div className="container">
-              <span className="loader__text">Loading...</span>
-            </div>
-        ):(
-          <div className="moveis">
-            {movies.map(movie => (
-              <Movie
-                key = {movie.id}
-                id = {movie.id}
-                year = {movie.year}
-                title = {movie.title}
-                summary = {movie.summary}
-                poster = {movie.medium_cover_image}
-                genres = {movie.genres}
-              />
-            ))}
-          </div>
-          )}
-      </section>
-      );
-    } 
-  }
+function App(){
+  return (
+    <HashRouter>
+      <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About} />
+    <Route path="/movie/:id" component={Detail}/>
+  </HashRouter>
+  ); 
+}
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,54 @@
+import React from "react";
+import axios from "axios";
+import Movie from "../components/Movie";
+import "./Home.css";
+
+class Home extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: 
+          { movies }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    // 첫번째는 state에서 온 movies이고 두 번째는 axios에서 온 movies이다.같은 movies이기 때문에 줄여서 { movies }라고 써준다.
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount(){
+    this.getMovies(); 
+  }
+
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="loader">
+        {isLoading ? (
+          <div className="container">
+              <span className="loader__text">Loading...</span>
+            </div>
+        ):(
+          <div className="moveis">
+            {movies.map(movie => (
+              <Movie
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+                genres = {movie.genres}
+              />
+            ))}
+          </div>
+          )}
+      </section>
+      );
+    } 
+  }
+
+export default Home;
